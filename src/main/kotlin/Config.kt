0cb00d2fce8d62261c8ed4181e749fa361d9e434
Luís Soares <lsoares@gmail.com>
2020-05-05 16:36:47 +0100

    Fix command line tool in MySQL

--- src/main/kotlin/Config.kt
+++ src/main/kotlin/Config.kt
@@ -6,7 +6,9 @@ object Config {
     private val database by lazy {
         Database.connect(url = System.getenv("MYSQL_URL"), driver = "com.mysql.cj.jdbc.Driver")
     }
-    val userRepoMySql by lazy { MySqlUserRepository(database).apply { createSchema() } }
+    val userRepoMySql by lazy {
+        MySqlUserRepository(database).apply { createSchema() }
+    }
     val userRepoMongoDb by lazy {
         MongoDBUserRepository(
             System.getenv("MONGODB_HOST"),
@@ -17,13 +19,14 @@ object Config {
 }
 
 /*
-    export MYSQL_URL=jdbc:mysql://localhost:3306/clean_demo
-    docker run --name mysql_demo -e MYSQL_ALLOW_EMPTY_PASSWORD=true -e MYSQL_DATABASE=clean_demo -p 3306:3306 -d mysql
-    docker stop mysql_demo
+    MYSQL_URL=jdbc:mysql://root:my-secret-pw@localhost:3306/clean_demo
+    docker stop mysql_demo && docker rm mysql_demo
+    docker run --name mysql_demo -e MYSQL_ROOT_PASSWORD=my-secret-pw -e MYSQL_DATABASE=clean_demo -p 3306:3306 -d mysql
  */
+
 /*
-    export MONGODB_HOST=localhost
-    export MONGODB_PORT=27017
-    docker run --name mongodb_demo -p 27017:27017 mongo
-    docker stop mongodb_demo
+    MONGODB_HOST=localhost
+    MONGODB_PORT=27017
+    docker stop mongodb_demo && docker rm mongodb_demo
+    docker run --rm --name mongodb_demo -p 27017:27017 mongo
  */
