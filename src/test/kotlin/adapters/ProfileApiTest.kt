57b3aa61c9a749b4d6fd10254d66752d2c725ba1
Team Falcon <team-falcon@vw-dilab.com>
2020-06-03 00:15:23 +0100

    Post a user profile

--- src/test/kotlin/adapters/ProfileApiTest.kt
+++ src/test/kotlin/adapters/ProfileApiTest.kt
@@ -3,22 +3,51 @@ package adapters
 import domain.Profile
 import domain.toEmail
 import io.javalin.Javalin
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.skyscreamer.jsonassert.JSONAssert
 
 class ProfileApiTest {
 
+    private lateinit var fakeProfile: Javalin
+
+    @AfterEach
+    fun `after each`() {
+        fakeProfile.stop()
+    }
+
     @Test
-    fun `gets the user profile by id`() {
-        val fakeProfile = Javalin.create().get("profile/abc") {
-            it.json(mapOf("id" to "abc", "email" to "x123@gmail.com"))
+    fun `gets a user profile by id`() {
+        fakeProfile = Javalin.create().get("profile/abc") {
+            // it.json(mapOf("id" to "abc", "email" to "x123@gmail.com"))
+            it.result(""" {"id": "abc", "email": "x123@gmail.com"} """)
+                .contentType("application/json")
         }.start(1234)
         val profileApi = ProfileApi(apiUrl = "http://localhost:1234")
 
         val result = profileApi.fetchProfile("abc")
 
         assertEquals(Profile(id = "abc", email = "x123@gmail.com".toEmail()), result)
+    }
 
-        fakeProfile.stop()
+    @Test
+    fun `posts a user profile`() {
+        var postedBody: String? = null
+        var contentType: String? = null
+        fakeProfile = Javalin.create().post("profile") {
+            postedBody = it.body()
+            contentType = it.contentType()
+            it.status(201)
+        }.start(1234)
+        val profileApi = ProfileApi(apiUrl = "http://localhost:1234")
+
+        profileApi.saveProfile(Profile(id = "abc", email = "x123@gmail.com".toEmail()))
+
+        JSONAssert.assertEquals(
+            """ { "id": "abc", "email": "x123@gmail.com"}  """,
+            postedBody, true
+        )
+        assertEquals("application/json", contentType)
     }
 }
